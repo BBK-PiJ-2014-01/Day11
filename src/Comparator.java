130a925b3f8d1f3d31a0afebcd51436c70b4e3e5
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-07 17:23:23 +0000

    No code repeat - code complete

--- src/Comparator.java
+++ src/Comparator.java
@@ -1,14 +1,23 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
 /**
  * Created by Pierre on 06/12/2014.
  */
 public class Comparator {
 
+    public static void main(String[] args) {
+        Comparator c = new Comparator();
+        c.run();
+    }
+
+    public void run() {
+        System.out.println(getMax(5, 7));
+        System.out.println(getMax(6.2,5.75));
+        System.out.println(getMax("34","42"));
+    }
+
     public int getMax(int n, int m) {
-        if (n > m) {
-            return n;
-        } else {
-            return m;
-        }
+        return((int) getMax((double)n,(double)m));
     }
     public double getMax(double d1, double d2) {
         if (d1 > d2) {
@@ -17,14 +26,11 @@ public class Comparator {
             return d2;
         }
     }
+
     public String getMax(String number1, String number2) {
         int n1 = Integer.parseInt(number1);
         int n2 = Integer.parseInt(number2);
-        if (n1 > n2) {
-            return number1;
-        } else {
-            return number2;
-        }
+        return(Integer.toString(getMax((int) n1, (int) n2)));
     }
 
 }
