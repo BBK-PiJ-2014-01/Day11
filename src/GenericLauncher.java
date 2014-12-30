276503f00fb63e42e5ded0c810f3b34ea1cfa6ac
BBK-PiJ-2014-01 <pmeyer02@mail.bbk.ac.uk>
2014-12-30 22:20:56 +0000

    Initial commit

---
+++ src/GenericLauncher.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Pierre on 30/12/2014.
+ */
+public class GenericLauncher {
+    public static void main(String[] args) {
+        GenericLauncher gl = new GenericLauncher();
+        gl.run();
+    }
+
+    public void run() {
+        
+    }
+}
